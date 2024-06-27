0827ebe5c6eb05e040c5bd8bc15e369581fcfb1b
Denis Chornyi <chornyi.den@gmail.com>
2024-06-27 17:08:47 +0300

    fix

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -3,10 +3,7 @@ import moment from "moment";
 export const getWeekStartDate = (date) => {
   const dateCopy = new Date(date);
   const dayOfWeek = dateCopy.getDay();
-  const difference =
-    dayOfWeek === 0
-      ? -6 // for Sunday
-      : 1 - dayOfWeek;
+  const difference = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
 
   const monday = new Date(dateCopy.setDate(date.getDate() + difference));
   return new Date(monday.getFullYear(), monday.getMonth(), monday.getDate());
